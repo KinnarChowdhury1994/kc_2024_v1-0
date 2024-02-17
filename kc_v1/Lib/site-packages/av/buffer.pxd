495d79ddbecc44ad34de74c2001b745d427046e4
kincho94 <kinnar.chowdhury@msqube.com>
2024-02-17 05:52:08 +0530

    removed env

--- kc_v1/Lib/site-packages/av/buffer.pxd
+++
@@ -1,6 +0,0 @@
-
-cdef class Buffer:
-
-    cdef size_t _buffer_size(self)
-    cdef void* _buffer_ptr(self)
-    cdef bint _buffer_writable(self)
--- kc_v1/Lib/site-packages/dj_db_conn_pool/compat/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    # A legacy compatibility wrapper for Unicode handling on Python 2.
-    # "ugettext_lazy" has been removed since Django 4.0.
-    from django.utils.translation import ugettext_lazy as gettext_lazy
-except ImportError:
-    # Django 4.0
-    from django.utils.translation import gettext_lazy
